21988a699afabf76c4d1901e3dc3f46c9d352e1a
skalutki <skalutki@google.com>
2020-05-10 17:14:57 +0100

    feat: JavaScript fetch repositories from github api

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,37 @@
 "use strict";
-/*
+
+console.log(`Welcom to my CV page. Witam na mojej stronie CV.`)
+
 const list = document.querySelector('.projects__list-js');
 
 fetch("https://api.github.com/users/skalutki/repos?sort=updated&direction=dest")
     .then(resp => resp.json())
     .then(resp => {
-        const repos = resp;
-        for (const repo of repos) {
-            const { html_url, name, description, homepage } = repo
+            const repos = resp;
+            for (const repo of repos) {
+                const { html_url, name, description, homepage } = repo
 
-            list.innerHTML += `
-            <li class="projects__list-item"> 
-              <section class="project">
-                <span class="project__icon1"></span> 
-                <h3 class="project__title">${name}</h3> 
-                <p class="project__paragraph">${description ? description : "brak opisu repozytorium"}</p>
-                <section class="link">
-                  <div class="link__box">
-                    <a class="link__box-item" href = "${homepage}"> <span class="link__box-icon2"></span>Demo</a> 
-                    <div class="link__box-separation"></div>
-                    <a class="link__box-item" href = "${html_url}"> <span class="link__box-icon3"></span>Github</a>
-                  </div>
-                </section>                
-              </section>
-            </li>
+                list.innerHTML += `
+            <li class="project">
+                        <div class="project__container">
+                            <img class="project__logo" src="../assets/img/GithubBlack.svg" alt="" width="48px" height="48px">
+                            <h3 class="project__title">${name}</h3>
+                            ${
+                              description ? `<p class="project__description">${description}</p>` : 'No description'
+                            }
+                            
+                        </div>
+                        <div class="project__footer">
+                        ${
+                          homepage ? `<a class="project__link project__link--demo" href=""${homepage}" target="_blank" rel="nofollor noreferrer" title="Demo: ${name}">Demo</a>` : '' 
+                        }
+                            
+                            <a class="project__link project__link--code" href="${html_url}" target="_blank" rel="nofollor noreferrer" title="Source code: ${name}"> GitHub</a>
+                        </div>
+                    </li>
                   `;
         }
     })
     .catch(err => {
         console.log(err);
-    })*/
+    })
